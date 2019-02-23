17bd6460966de78ff214969644543b5000c882e4
aburillo <aburillo@ucm.es>
2019-02-23 18:13:05 +0100

    pruebas

--- sample_gunicorn_config.py
+++ sample_gunicorn_config.py
@@ -4,9 +4,9 @@ proc_name = 'WikiChron'
 loglevel = 'debug'
 errorlog = '-'
 workers = 3 
-raw_env = [
-	"WIKICHRON_DATA_DIR=/var/wiki_dumps/csv/"
-]
+#raw_env = [
+#	"WIKICHRON_DATA_DIR=/var/wiki_dumps/csv/"
+#]
 
 
 
