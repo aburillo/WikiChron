947cc761156352567b773ad4a5a61063ae107814
aburillo <aburillo@ucm.es>
2019-02-23 18:06:43 +0100

    prueba cambio deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git pull origin master &&
-if [ -e venv.old/ ]; then
-  rm -r venv.old/
-fi  
-mv venv/ venv.old/ &&
+git pull origin master #&&
+#if [ -e venv.old/ ]; then
+#  rm -r venv.old/
+#fi  
+#mv venv/ venv.old/ &&
 virtualenv -p python3 venv/ &&
 source venv/bin/activate &&
 pip install -r requirements.txt &&
